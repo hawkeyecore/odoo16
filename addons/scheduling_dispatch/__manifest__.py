30a33ed58b2ac712191ced5bd2300299ae130b84
hawkeyecore <hawkeye20192@gmail.com>
2025-02-25 02:29:23 -0600

    change hide brand name

--- addons/scheduling_dispatch/__manifest__.py
+++ addons/scheduling_dispatch/__manifest__.py
@@ -24,6 +24,7 @@
             'scheduling_dispatch/static/src/js_inherit.xml',
             'scheduling_dispatch/static/src/js/favicon.js',
             '/scheduling_dispatch/static/src/css/project_monitor.css',
+            'scheduling_dispatch/static/src/css/custom_style.css',
             # '/scheduling_dispatch/static/lib/chart.js',
             # '/scheduling_dispatch/static/src/js/project_monitor_chart.js',
         ],
