5bac5719ff619d4779c7f2978fe18e684a10f1d5
hawkeyecore <hawkeye20192@gmail.com>
2025-02-24 23:18:40 -0600

    Update name

--- addons/TaskMeToday_app/models/show_custom_app.py
+++ addons/TaskMeToday_app/models/show_custom_app.py
@@ -6,5 +6,5 @@ class ModuleModule(models.Model):
     @api.model
     def _search(self, domain, offset=0, limit=None, order=None, count=False):
         """ Modify the search to filter out marketplace apps. """
-        domain.append(("author", "=", "TaskMeToday"))  # Change 'Your Company' to match your custom apps' author
+        domain.append(("author", "=", "TaskMeToday App"))  # Change 'Your Company' to match your custom apps' author
         return super(ModuleModule, self)._search(domain, offset, limit, order, count)
